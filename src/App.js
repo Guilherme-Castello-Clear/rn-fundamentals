b83200fdd3433febde2c866a6985739bb9dd8960
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-08-17 16:10:07 -0300

    Feat: Using Styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+
+import Primeiro from "./Componentes/Primeiro";
+import { Comp1, Comp2 } from "./Componentes/Multi";
+import Comp from "./Componentes/Multi";
 
 export default () => {
-    return(
-        <View>
-            <Text>Primeiro Componente</Text>
+    
+    return (
+        <View style={style.App}>
+            <Comp/>
+            <Comp1/>
+            <Comp2/>
         </View>
     )
 }
+
+const style = StyleSheet.create({
+    
+    App: {
+        flexGrow: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    }
+})
---
+++ src/Componentes/Multi.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+function Comp() {
+    return <Text>Comp Principal</Text>
+}
+
+function Comp1() {
+    return <Text>Comp 1</Text>
+}
+
+function Comp2() {
+    return <Text>Comp 2</Text>
+}
+
+export default Comp
+export { Comp1, Comp2 }
